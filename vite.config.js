565d16194ffed5ea016e2f1970f4b37c616e06f7
jemson sayre <jemsonsayre@gmail.com>
2026-03-21 15:17:51 +0800

    [Featured] Auth

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -8,23 +7,5 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    
-    resolve: {
-        alias: {
-            '@': '/resources/js',
-        }
-    },
-
-    server: {
-        host: "www.sandbox.vitalmetrics.com",
-        port: 5173,
-        hmr:{
-            host: "www.sandbox.vitalmetrics.com"
-        },
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
 });
